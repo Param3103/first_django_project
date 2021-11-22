92d0137fa22b54cdfe39f6f500ec5d534c19cd99
Param3103 <param.mehrotra@gmail.com>
2021-11-22 20:17:34 +0800

    .

--- storefront/settings.py
+++ storefront/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nation',
         'HOST': 'localhost',
         'USERNAME': 'root',

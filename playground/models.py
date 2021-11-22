fe52793d180509d1ecc1b5dac20e8cb2ca1bc6b8
Param3103 <param.mehrotra@gmail.com>
2021-11-22 20:13:12 +0800

    .

--- playground/models.py
+++ playground/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import datetime
+import os
 
+def filepath(request, filename):
+    old_filename = filename
+    timeNow  = datetime.datetime.now()
+    fileName = "%s%s", (timeNow, old_filename)
+    return os.path.join("uploads/", filename)
 # Create your models here.
+class Item(models.Model):
+    name = models.TextField(max_length=2700)
+    price = models.TextField(max_length=2700)
+    description = models.TextField(max_length=2700)
